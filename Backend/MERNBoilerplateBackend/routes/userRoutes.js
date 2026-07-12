bb7752183bc2bf106aeac5fc86a88b674fb867ee
harsh91460 <harshmishra9146@gmail.com>
2026-07-12 09:59:08 +0530

    Delete Backend/MERNBoilerplateBackend directory

--- Backend/MERNBoilerplateBackend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getUserById } from "../controllers/userController.js";
-
-const userRouter = express.Router();
-
-userRouter.get("/:id", getUserById);
-
-export default userRouter;
